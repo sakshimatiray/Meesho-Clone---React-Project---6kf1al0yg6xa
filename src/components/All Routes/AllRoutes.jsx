add2dc2661e7de88ad8dfce8a3a1dc7cdcf1fe94
sakshi <sakshimatiray96628@gmail.com>
2023-01-23 00:07:22 +0530

    first

---
+++ src/components/All Routes/AllRoutes.jsx
@@ -0,0 +1,70 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import React from "react";
+import Home from "../Home/Home";
+import Products from "../Products/Products";
+
+import Product from "../Product/Product";
+import Cart from "../Cart/Cart";
+import Address from "../Address/Address";
+import Payment from "../Payment/Payment";
+import Summary from "../Summary/Summary";
+import Profile from "../Profile/Profile";
+import Login from "../Profile/Login";
+import Footer from "../Footer/Footer";
+import Checkout from "../Checkout/Checkout";
+
+export default function AllRoutes() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+     0    <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/products/:type" element={<Products />} />
+      <Route path="/product/:id" element={<Product />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/checkout/cart"
+          element={
+            <>
+              <Checkout />
+              <Cart />
+            </>
+          }
+        />
+        <Route
+          path="/checkout/address"
+          element={
+            <>
+              <Checkout />
+              <Address />
+            </>
+          }
+        />
+        <Route
+          path="/checkout/payment"
+          element={
+            <>
+              <Checkout />
+              <Payment />
+            </>
+          }
+        />
+        <Route
+          path="/checkout/summary"
+          element={
+            <>
+              <Checkout />
+              <Summary />
+            </>
+          }
+        />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/Navbar/data.js
@@ -0,0 +1,13 @@
+let listItems = [
+  "men's clothing",
+  "women's clothing",
+  "jewelery",
+  "electronics",
+  "Beauty & Health",
+  "Bags & Footwear",
+  "Home & Kitchen",
+  "Kids",
+  "Electronics",
+];
+
+export { listItems};
